b8b4d0f86e4981ff6ab588ee2743ce45d7c745af
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-09-11 15:45:23 -0400

    add encryption to user-PUT password

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -73,7 +73,7 @@ router.put("/", validateSession, (req, res) => {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.password,
+    password: bcrypt.hashSync(req.body.password, 11),
   };
   const query = { where: {id: userid} };
   User.update(updateUser, query)
