9589be8e99c2aedbef3ebd853485bdf75dfaa7bd
Amber Janzen <janzenamber@gmail.com>
2020-09-11 10:49:45 -0400

    created media post

---
+++ controllers/mediacontroller.js
@@ -0,0 +1,29 @@
+let express = require ('express');
+let router = express.Router();
+let validateSession = require('../middleware/validate-session');
+const Media = require('../db').import('../models/media');
+
+
+// router.get('/media', validateSession, function(req, res)
+// {
+//     res.send('test route for media')
+// })
+// POST: http://localhost:3025/media/create
+
+router.post('/create', validateSession, (req,res) =>{
+    const mediaEntry = {
+        type: req.body.media.type,
+        title: req.body.media.title,
+        genre: req.body.media.genre,
+        description: req.body.media.description,
+        rating: req.body.media.rating,
+        consumed: req.body.media.consumed,
+        platform: req.body.media.platform,
+        userId: req.user.id
+    }
+    Media.create(mediaEntry)
+    .then(media => res.status(200).json(media))
+    .catch(err => res.status(500).json({ error: err}))
+});
+
+module.exports = router
