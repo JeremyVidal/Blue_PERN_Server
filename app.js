7c99ffca3810a0edd36b84385ab41037641fd862
Jeremy <jbvidal101@gmail.com>
2020-09-11 09:47:02 -0400

    Updated the app.js to fix some errors.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express');
-
-
-
-
-
-
 const app = express();
+
+
+const sequelize = require('./db');
+sequelize.sync();
+
+
+
+
+
 
 
 
@@ -14,11 +17,16 @@ const app = express();
 
 
 app.use(express.json());
+app.use(require('./middleware/headers'));
+
+// let user = require('./controllers/usercontroller');
+// app.use('/user', user);
+// let media = require('./controllers/mediacontroller');
+// app.use('/media', media);
 
 
-app.use('/user', user);
 
-app.use('/media', media);
+
 
 
 
