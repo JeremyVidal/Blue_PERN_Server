e932f7d586439f3314f240035233217ac384c4c6
Jeremy <jbvidal101@gmail.com>
2020-09-14 13:29:04 -0400

    daily update to get everyones data

--- controllers/mediacontroller.js
+++ controllers/mediacontroller.js
@@ -35,7 +35,7 @@ router.post('/create', validateSession, (req,res) =>{
 router.get("/", validateSession, (req, res) => {
     let userid = req.user.id;
     Media.findAll({
-      where: {id: userid },
+      where: {userId: userid },
     })
       .then((media) => res.status(200).json(media))
       .catch((err) => res.status(500).json({ error: err }));
@@ -49,19 +49,20 @@ router.get("/all", (req, res) => {
   });
 
   // -----  Update Media  -----  
-  // PUT:   http://localhost:3025/media/:id
+  // PUT:   http://localhost:3025/media/update/:id
   router.put('/update/:id', (req, res) => {
       const updateMediaEntry = {
           type: req.body.media.type,
           title: req.body.media.title,
           genre: req.body.media.genre,
-          description: req.body.media.description,
+		  description: req.body.media.description,
+          rated: req.body.media.rated,
           rating: req.body.media.rating,
           consumed: req.body.media.consumed,
           platform: req.body.media.platform,
         };
         
-        const query = { where: { id: req.params.entryId, userId: req.user.id } };
+        const query = { where: { id: req.params.id } };
         
         Media.update(updateMediaEntry, query)
         .then((media) => res.status(200).json(media))
