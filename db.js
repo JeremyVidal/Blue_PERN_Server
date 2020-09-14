f907bfff453fe2774b5ac95f3e540692784bbc13
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-09-14 11:52:39 -0400

    update Heroku connection

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('media','postgres', process.env.PASS, {
-host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// host: 'localhost',
 dialect: 'postgres'
 });
 
