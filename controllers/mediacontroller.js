c3872ca7e71e863faf572db9c039cc3289ca35c3
Patrick Walls <walls.patrick.a@gmail.com>
2020-09-14 22:32:17 -0400

    Fixed Media PUT, all references to userId have to match the media model

--- controllers/mediacontroller.js
+++ controllers/mediacontroller.js
@@ -1,8 +1,7 @@
-let express = require ('express');
+let express = require("express");
 let router = express.Router();
-let validateSession = require('../middleware/validate-session');
-const Media = require('../db').import('../models/media');
-
+let validateSession = require("../middleware/validate-session");
+const Media = require("../db").import("../models/media");
 
 // Endpoints
 // POST:  http://localhost:3025/media/create
@@ -11,70 +10,68 @@ const Media = require('../db').import('../models/media');
 // PUT:   http://localhost:3025/media/:id
 // DEL:   http://localhost:3025/media/:id
 
-
 // -----  Media Create  -----
 // POST:  http://localhost:3025/media/create
-router.post('/create', validateSession, (req,res) =>{
-    const mediaEntry = {
-        type: req.body.media.type,
-        title: req.body.media.title,
-        genre: req.body.media.genre,
-        description: req.body.media.description,
-        rated: req.body.media.rated,
-        rating: req.body.media.rating,
-        consumed: req.body.media.consumed,
-        platform: req.body.media.platform,
-        userId: req.user.id
-    }
-    Media.create(mediaEntry)
-    .then(media => res.status(200).json(media))
-    .catch(err => res.status(500).json({ error: err}))
+router.post("/create", validateSession, (req, res) => {
+  const mediaEntry = {
+    type: req.body.media.type,
+    title: req.body.media.title,
+    genre: req.body.media.genre,
+    description: req.body.media.description,
+    rated: req.body.media.rated,
+    rating: req.body.media.rating,
+    consumed: req.body.media.consumed,
+    platform: req.body.media.platform,
+    userId: req.user.id,
+  };
+  Media.create(mediaEntry)
+    .then((media) => res.status(200).json(media))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 // -----Get My Media  -----
 // GET:   http://localhost:3025/media/
 router.get("/", validateSession, (req, res) => {
-    let userid = req.user.id;
-    Media.findAll({
-      where: {id: userid },
-    })
-      .then((media) => res.status(200).json(media))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
+  let userid = req.user.id;
+  Media.findAll({
+    where: { id: userid },
+  })
+    .then((media) => res.status(200).json(media))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 // -----  Get All Media -----
 // GET:   http://localhost:3025/media/all
 router.get("/all", (req, res) => {
-    Media.findAll()
-      .then((media) => res.status(200).json(media))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
+  Media.findAll()
+    .then((media) => res.status(200).json(media))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
-  // -----  Update Media  -----  
-  // PUT:   http://localhost:3025/media/:id
-  router.put('/update/:id', (req, res) => {
-      const updateMediaEntry = {
-          type: req.body.media.type,
-          title: req.body.media.title,
-          genre: req.body.media.genre,
-          description: req.body.media.description,
-          rating: req.body.media.rating,
-          consumed: req.body.media.consumed,
-          platform: req.body.media.platform,
-        };
-        
-        const query = { where: { id: req.params.entryId, userId: req.user.id } };
-        
-        Media.update(updateMediaEntry, query)
-        .then((media) => res.status(200).json(media))
-        .catch((err) => res.status(500).json({ error: err }));
-    });
-    
-    // -----  Delete a Media Entry  -----
-    // DEL:   http://localhost:3025/media/:id
-	router.delete('/:id', (req, res) => {
-		Media.destroy({where: { id: req.params.id }})
-		.then(media => res.status(200).json(media))
-		.catch(err => res.json(req.errors))
-	})
-	
+// -----  Update Media  -----
+// PUT:   http://localhost:3025/media/:id
+router.put("/update/:id", validateSession, (req, res) => {
+  const updateMediaEntry = {
+    type: req.body.media.type,
+    title: req.body.media.title,
+    genre: req.body.media.genre,
+    description: req.body.media.description,
+    rating: req.body.media.rating,
+    consumed: req.body.media.consumed,
+    platform: req.body.media.platform,
+  };
+
+  const query = { where: { id: req.params.id, userId: req.user.id } };
+
+  Media.update(updateMediaEntry, query)
+    .then((media) => res.status(200).json(media))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+// -----  Delete a Media Entry  -----
+// DEL:   http://localhost:3025/media/:id
+router.delete("/:id", (req, res) => {
+  Media.destroy({ where: { id: req.params.id } })
+    .then((media) => res.status(200).json(media))
+    .catch((err) => res.json(req.errors));
+});
+
 module.exports = router;
-
