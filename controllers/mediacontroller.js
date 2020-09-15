8293531a3dd9bca56a5f3aa978a5cbe8fc96cfcd
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-09-15 09:32:57 -0400

    update media get

--- controllers/mediacontroller.js
+++ controllers/mediacontroller.js
@@ -32,7 +32,8 @@ router.post('/create', validateSession, (req,res) =>{
 });
 // -----Get My Media  -----
 // GET:   http://localhost:3025/media/
-router.get("/", validateSession, (req, res) => {
+// router.get("/", validateSession, (req, res) => {
+router.get("/", (req, res) => {
     let userid = req.user.id;
     Media.findAll({
       where: {userId: userid },
